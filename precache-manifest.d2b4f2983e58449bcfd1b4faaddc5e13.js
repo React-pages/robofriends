ae397414ff3c56a848e65b9dfef41d01a7160294
Akari Nishii <nishii0731@gmail.com>
2019-01-23 14:07:35 +0100

    Updates

---
+++ precache-manifest.d2b4f2983e58449bcfd1b4faaddc5e13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "93e690efce357ad2bb27",
+    "url": "/robofriends/static/js/main.93e690ef.chunk.js"
+  },
+  {
+    "revision": "09f0b8b1d9adc74fe3c0",
+    "url": "/robofriends/static/js/1.09f0b8b1.chunk.js"
+  },
+  {
+    "revision": "93e690efce357ad2bb27",
+    "url": "/robofriends/static/css/main.6b8d6993.chunk.css"
+  },
+  {
+    "revision": "09f0b8b1d9adc74fe3c0",
+    "url": "/robofriends/static/css/1.8a9d23fb.chunk.css"
+  },
+  {
+    "revision": "0846268441f3bbf67475ddae369b0bd3",
+    "url": "/robofriends/index.html"
+  }
+];
